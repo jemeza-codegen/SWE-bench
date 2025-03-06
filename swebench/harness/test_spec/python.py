ea9e6c695b7accc24851afecab94b125b087517e
jemeza-codegen <jmeza@codegen.com>
2025-03-06 14:19:49 -0800

    Revert "Merge pull request #1 from jemeza-codegen/jmeza-test-parse-fix"
    This reverts commit 14ce4fc3507b66cdb5dd27ef0722021e4ddab54d, reversing
    changes made to b8084edba022be47912d2a21eba5f01ac3990da3.

--- swebench/harness/test_spec/python.py
+++ swebench/harness/test_spec/python.py
@@ -308,9 +308,9 @@ def make_eval_script_list_py(
     eval_commands += [
         reset_tests_command,
         apply_test_patch_command,
-        f"echo '{START_TEST_OUTPUT}'", # allows for astropy test output to be parsed
+        f": '{START_TEST_OUTPUT}'",
         test_command,
-        f"echo '{END_TEST_OUTPUT}'", # allows for astropy test output to be parsed
+        f": '{END_TEST_OUTPUT}'",
         reset_tests_command,  # Revert tests after done, leave the repo in the same state as before
     ]
     return eval_commands
